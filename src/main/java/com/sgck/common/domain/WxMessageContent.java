4286b2fa43b330288cc01137e79501fa40012112
cang900417 <awb900417>
2017-06-27 11:27:45 +0800

    添加颜色属性

--- src/main/java/com/sgck/common/domain/WxMessageContent.java
+++ src/main/java/com/sgck/common/domain/WxMessageContent.java
@@ -4,46 +4,52 @@ import java.io.Serializable;
 
 import com.alibaba.fastjson.JSONObject;
 
-public class WxMessageContent implements Serializable
-{
+public class WxMessageContent implements Serializable {
 
-	private String templateId;//模板ID
-	private String first;//标题
-	private String remark;//支持换行
-	private JSONObject content;//{keyword1:xx,keyword2:xx} 
-	
-	public String getTemplateId()
-	{
+	private String templateId;// 模板ID
+	private String first;// 标题
+	private String remark;// 支持换行
+	private JSONObject content;// {keyword1:xx,keyword2:xx}
+	private JSONObject color;// 颜色
+
+	public String getTemplateId() {
 		return templateId;
 	}
-	public void setTemplateId(String templateId)
-	{
+
+	public void setTemplateId(String templateId) {
 		this.templateId = templateId;
 	}
-	public String getFirst()
-	{
+
+	public String getFirst() {
 		return first;
 	}
-	public void setFirst(String first)
-	{
+
+	public void setFirst(String first) {
 		this.first = first;
 	}
-	public String getRemark()
-	{
+
+	public String getRemark() {
 		return remark;
 	}
-	public void setRemark(String remark)
-	{
+
+	public void setRemark(String remark) {
 		this.remark = remark;
 	}
-	public JSONObject getContent()
-	{
+
+	public JSONObject getContent() {
 		return content;
 	}
-	public void setContent(JSONObject content)
-	{
+
+	public void setContent(JSONObject content) {
 		this.content = content;
 	}
-	
-	
+
+	public void setColor(JSONObject colorJson) {
+		this.color = colorJson;
+	}
+
+	public JSONObject getColor() {
+		return color;
+	}
+
 }
